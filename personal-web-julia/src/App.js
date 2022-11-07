49a78dd642e3e961cad8c3911e15c1da504f500d
juliaarisanda <juliaarisanda12@gmail.com>
2022-11-08 00:19:10 +0700

    Tambah Blog Page

--- personal-web-julia/src/App.js
+++ personal-web-julia/src/App.js
@@ -6,6 +6,7 @@ import Banner  from "./components/Banner";
 import About from "./components/About";
 import {Portfolio} from "./components/Portfolio";
 import { Footer } from "./components/Footer";
+import Blog from "./components/Blog";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Banner/>
       <About/>
       <Portfolio/>
+      <Blog/>
       <Footer/>
     </div>
   );
